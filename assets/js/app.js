e386322b9168fe31252f0eb26d94d3a5366780bb
jkaminsky2 <102625537+jkaminsky2@users.noreply.github.com>
2024-04-18 18:18:24 -0700

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,3 @@
-/* -----------------------------------------------
-/* How to use? : Check the GitHub README
-/* ----------------------------------------------- */
 
 /* To load a config file (particles.json) you need to host this demo (MAMP/WAMP/local)... */
 /*
